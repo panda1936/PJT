58f9cbe26787ffae39a84783f781ca62a357fcf5
Olivier611 <olivier611.chen@gmail.com>
2020-05-14 22:33:14 +0200

    Mise à jour

--- PagePrincipaleV4/index_test.php
+++ PagePrincipaleV4/index_test.php
@@ -189,7 +189,7 @@ if(!empty($_POST['addColumn'])){
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>Study School</title>
+        <title>Studybo</title>
         <link rel="icon" href="icone.png" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
